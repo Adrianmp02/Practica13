1974f90afabd411c2d33f5cfeea249643449cfe7
DavidIglesias69 <david.iglcua@educa.jcyl.es>
2023-11-21 13:35:19 +0100

    Genero primitiva Random

--- Practica13/src/PracticaFinal/David.java
+++ Practica13/src/PracticaFinal/David.java
@@ -1,5 +1,35 @@
 package PracticaFinal;
 
-public class David {
+import java.util.Random;
 
+public class David {
+	
+
+	//David o Moises
+	public static int[] primitivaRandom(){
+		int [] primitivaR = new int [6];
+		
+		for (int n = 0; n < primitivaR.length; n++) {
+			primitivaR[n] = numeroAleatorio();
+		}
+		
+		return primitivaR;
+
+	}
+	public static int primitivaReintegro () {
+		int min = 0;
+		int max = 9;
+		
+		Random rn = new Random();
+		return rn.nextInt(max - min + 1) + min;
+	}
+
+
+	public static int numeroAleatorio(){
+		int min = 1;
+		int max = 20;
+		
+		Random rn = new Random();
+		return rn.nextInt(max - min + 1) + min;
+	}
 }
--- Practica13/src/PracticaFinal/Main.java
+++ Practica13/src/PracticaFinal/Main.java
@@ -1,5 +1,17 @@
 package PracticaFinal;
 
+import java.util.Arrays;
+
 public class Main {
 
+	
+	public static void main(String[] args) {
+		int rPri[] = David.primitivaRandom();
+		
+		
+		System.out.println(Arrays.toString(rPri));
+		
+		System.out.println(David.primitivaReintegro());
+	}
+	
 }
