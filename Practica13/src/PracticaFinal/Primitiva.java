af2be0d8a24411cf8961b0507afdc1dc13bbc268
Adrianmp02 <adrian.marper.6@educa.jcyl.es>
2023-11-21 12:28:43 +0100

    Trabajo Asignado
    Creamos metodos y dividimos

---
+++ Practica13/src/PracticaFinal/Primitiva.java
@@ -0,0 +1,43 @@
+package PracticaFinal;
+
+public class Primitiva {
+
+	
+	//Se pueden hacer cambios (tipo cambio void a otra cosa etc)
+	
+	//David o Moises
+	public void miPrimitiva() {
+		
+		
+		
+	}
+	
+	
+	//David o Moises
+	public void primitivaRandom(){
+		
+		
+		
+	}
+	
+	//Adrian
+	public void comparativa() {
+		
+		
+		
+	}
+	
+	//Alex
+	public void puntuaje() {
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
