6b843f6f78b6bfadd31e8af256e1d70a35dc7cb0
Adrianmp02 <adrian.marper.6@educa.jcyl.es>
2023-11-29 12:47:26 +0100

    Added class
    Añado clase nueva para orientar a objetos

---
+++ Practica13/src/PracticaFinal/CombObj.java
@@ -0,0 +1,22 @@
+package PracticaFinal;
+
+public class CombObj {
+
+	private int[] primitiva;
+	private int reintegro;
+	
+	public CombObj() {
+		
+		this.primitiva = new int [6];
+		this.reintegro = 0;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
